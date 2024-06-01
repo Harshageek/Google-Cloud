48224d512740a17599d357164052bca44f2930ae
Harshageek <92079592+Harshageek@users.noreply.github.com>
2024-06-01 20:36:21 +0530

    Create harsha.sh

---
+++ Cloud SQL with Terraform/harsha.sh
@@ -0,0 +1,13 @@
+mkdir sql-with-terraform
+cd sql-with-terraform
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+
+unzip gsp234.zip
+
+cat main.tf
+
+terraform init
+
+terraform plan -out=tfplan
+
+terraform apply tfplan
